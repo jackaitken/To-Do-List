93fa731775301d5bb59240df6ea6d2cfbd95d7a4
Jack Aitken <jaitk11@gmail.com>
2021-08-24 21:01:00 -0400

    testing with jest

--- to_do_list.js
+++ to_do_list.js
@@ -1,3 +1,5 @@
+const Todo = require('./todo');
+
 class TodoList {
   constructor(title) {
     this.title = title;
@@ -111,52 +113,4 @@ class TodoList {
   }
 }
 
-class Todo {
-  static DONE_MARKER = 'X';
-  static UNDONE_MARKER = ' ';
-
-  constructor(title) {
-    this.title = title;
-    this.done = false;
-  }
-
-  toString() {
-    let marker = this.isDone() ? Todo.DONE_MARKER : Todo.UNDONE_MARKER;
-    return `[${marker}] ${this.title}`;
-  }
-
-  markDone() {
-    this.done = true;
-  }
-
-  markUndone() {
-    this.done = false;
-  }
-
-  isDone() {
-    return this.done;
-  }
-
-  getTitle() {
-    return this.title;
-  }
-}
-
-let todo1 = new Todo("Buy milk");
-let todo2 = new Todo("Clean room");
-let todo3 = new Todo("Go to the gym");
-let todo4 = new Todo("Go shopping");
-let todo5 = new Todo("Feed the cats");
-let todo6 = new Todo("Study for Launch School");
-let list = new TodoList("Today's Todos");
-
-list.add(todo1);
-list.add(todo2);
-list.add(todo3);
-list.add(todo4);
-list.add(todo5);
-list.add(todo6);
-todo1.markDone();
-todo5.markDone();
-
-console.log(list.toArray());
+module.exports = TodoList;
